2a0bda2ce4544b3cc820db0d1aaaf8f155bf09f2
Akash Sakhare <akashsakhare18@gmail.com>
2022-10-08 18:56:17 +0530

    Updated Arithmatic.java

--- Arithmatic.java
+++ Arithmatic.java
@@ -2,6 +2,7 @@ public class Arithmatic {
     public static void main(String args[]) {
         int a = 4;
         int b = 5;
-        System.out.println("Sum: "+a+b);
+        System.out.println("Sum is: "+(a+b));
+        System.out.println("Difference is: "+(a-b));
     }
 }
