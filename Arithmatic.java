1afea2d00d42f926587768b622240fb81c240951
Akash Sakhare <akashsakhare18@gmail.com>
2022-10-08 20:49:12 +0530

    Added Multiplication(Arithmatic.java)

--- Arithmatic.java
+++ Arithmatic.java
@@ -4,5 +4,6 @@ public class Arithmatic {
         int b = 5;
         System.out.println("Sum is: "+(a+b));
         System.out.println("Difference is: "+(a-b));
+        System.out.println("Multiplication is: "+(a*b));
     }
 }
