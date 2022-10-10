e1f5ee61e786f78e20e695c7b225a79615b5aa13
Akash Sakhare <112418224+sakhareas@users.noreply.github.com>
2022-10-10 22:46:53 +0530

    added PrintWriter and Division Operation

--- Arithmatic.java
+++ Arithmatic.java
@@ -1,9 +1,13 @@
+import java.io.PrintWriter;
 public class Arithmatic {
     public static void main(String args[]) {
+        PrintWriter pw = new PrintWriter(System.out);
         int a = 4;
         int b = 5;
-        System.out.println("Sum is: "+(a+b));
-        System.out.println("Difference is: "+(a-b));
-        System.out.println("Multiplication is: "+(a*b));
+        pw.println("Sum is: "+(a+b));
+        pw.println("Difference is: "+(a-b));
+        pw.println("Multiplication is: "+(a*b));
+        pw.println("Division is: "+(a/b));
+        pw.close();
     }
 }
